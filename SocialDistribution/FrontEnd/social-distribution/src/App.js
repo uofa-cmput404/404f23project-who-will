951dfc149bef22e78d79f50ea1452153599c4f64
unknown <ziang6@ualberta.ca>
2023-10-12 00:10:06 -0600

    play with React, creating Button component

---
+++ SocialDistribution/FrontEnd/social-distribution/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Button from './Components/Button';
+function App() {
+  return (
+    <React.Fragment>
+      <h1
+      style={{
+        textAlign: "center",
+        fontSize: "17px",
+        margin: "15px"
+      }}
+      >
+        Create Reusable Button Components
+      </h1>
+      <div
+        style={{ 
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "10px",
+         }}
+      >
+        <Button variant="primary">Success Button</Button>
+        <Button>Danger Button</Button>
+        <Button>Warning Button</Button>
+        <Button>Primary Button</Button>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
