2a6509c8d8f5b03b7d917834eb8332772dd7cf12
unknown <ziang6@ualberta.ca>
2023-10-21 13:57:00 -0600

    restart backend

--- SocialDistribution/BackEnd/social_distribution/social_networking/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Post
-
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('author', 'content', 'timestamp', 'visibility')
-    # Other admin options
-
-admin.site.register(Post, PostAdmin)
