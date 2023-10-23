5fe59398af63c48dc6c3903a39467ca266ef8a2e
Yanmachine <hardingiant@gmail.com>
2023-10-23 12:40:32 -0600

    Popups
    settings popup and add post popups

--- SocialDistribution/FrontEnd/social-distribution/src/pages/Account.jsx
+++ SocialDistribution/FrontEnd/social-distribution/src/pages/Account.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './Account.css';
 import { FaGear, IconName } from "react-icons/fa6";
+import AddPost from './account/AddPost.jsx'
+import photo from '../images/free_profile_picture.png'; //need to import local images, I wonder how this will work for django database
+import Settings from './account/Settings';
 
 class Account extends Component {
 
@@ -11,24 +14,29 @@ class Account extends Component {
             {
                 id: 4,
                 content: "Newest post",
+                visibility: "private",
                 timestamp: "2023-10-18 16:38:00",
                 image: "https://reactjs.org/logo-og.png",
             },
             {
                 id: 3,
                 content: "post --------------------------------------- how long should be our max post length???????\n ABCDEFGHHIJKLMOPNW",
+                visibility: "public",
                 timestamp: "2023-10-18 10:00:00",
             },
             {
                 id: 2,
                 content: "Older Post",
+                visibility: "friends only",
                 timestamp: "2023-10-18 9:15:00",
                 image: "https://static.djangoproject.com/img/logos/django-logo-negative.png",
             },
             {
                 id: 1,
                 content: "Oldest Post",
+                visibility: "public",
                 timestamp: "2023-10-18 9:10:00",
+                image: photo,
             },
 
         ],
@@ -38,26 +46,67 @@ class Account extends Component {
     state = {
         user: null,
         error: null,
+        isSettingsOpen: false,
+        userSettings: null,
+        isAddPostOpen: false,
+        isSettingsOpen: false,
     };
 
-    //TODO add logic to the 3 buttons
+    handleSettingsClick = () => {
+        this.setState({ isSettingsOpen: true });
+    };
+
+    handleCloseSettings = () => {
+        this.setState({ isSettingsOpen: false });
+    };
+
     handleAddPost = () => {
-        console.log('Add post button clicked -- account');
+        this.setState({ isAddPostOpen: true });
     }
+
+    handleCloseAddPost = () => {
+        this.setState({ isAddPostOpen: false, content: '', visibility: 'public' });
+    }
+
+    addNewPost = (newPost) => {
+        this.setState((prevState) => ({
+            user: {
+                ...prevState.user,
+                posts: [...prevState.user.posts, newPost],
+            },
+        }));
+    };
 
     handleEditPost = () => {
         console.log('Edit post button clicked -- account');
     }
 
     handleDeletePost = (postId) => {
-        const updatedPosts = this.state.user.posts.filter(post => post.id !== postId); //new list with all posts except specified (deleted)
-        const updatedUser = { ...this.state.user, posts: updatedPosts }; 
-        this.setState({ user: updatedUser });
+        const confirmation = window.confirm("Are you sure you want to delete this post?");
+        
+        if (confirmation){
+            const updatedPosts = this.state.user.posts.filter(post => post.id !== postId); //new list with all posts except specified (deleted)
+            const updatedUser = { ...this.state.user, posts: updatedPosts }; 
+            this.setState({ user: updatedUser });
+        }
     }
 
     handleSettingsClick = () => {
-        //settings logic
-      }
+        this.openSettings();
+    };
+
+    openSettings = () => {
+        // Retrieve user settings data
+        const userSettings = {
+
+        };
+    
+        this.setState({ isSettingsOpen: true, userSettings });
+    };
+    
+    closeSettings = () => {
+        this.setState({ isSettingsOpen: false });
+    };
 
     componentDidMount() {
         // Retrieve user data from Django API
@@ -92,18 +141,33 @@ class Account extends Component {
                 <button className="settings-icon" onClick={this.handleSettingsClick}>
                     <FaGear/>
                 </button>
+                {this.state.isSettingsOpen && (
+                    <Settings onClose={this.handleCloseSettings} />
+                )}
 
                 <button className="add-post-button" onClick={this.handleAddPost}>Add Post</button> 
+                
+                {this.state.isAddPostOpen && (
+                    <AddPost
+                        onClose={this.handleCloseAddPost}
+                        onAddPost={this.addNewPost}
+                        image={this.state.image}
+                    />
+                )}
 
                 {/*Posts*/}
                 <div className="post-content">
                     {user.posts.map(post => (   //map method iterates over posts, current post is passed to arrow function
                         <div className="post-box" key={post.id}>
+                            <p id="visibility">Visibility: {post.visibility}</p>
                             <p>{post.content}</p>
                             {post.image && <img className="post-image"src={post.image} alt="Post Image" />}
                             <p>Posted on: {post.timestamp}</p>
-                            <button className="edit-post-button" onClick={this.handleEditPost}>Edit</button>
-                            <button className="delete-post-button" onClick={() => this.handleDeletePost(post.id)}>Delete</button>
+                            <div>
+                                <button className="comments-button" onClick={this.handleComments}>Comments</button>
+                                <button className="edit-post-button" onClick={this.handleEditPost}>Edit</button>
+                                <button className="delete-post-button" onClick={() => this.handleDeletePost(post.id)}>Delete</button>
+                            </div>
                         </div>
                     ))}
                 </div>
