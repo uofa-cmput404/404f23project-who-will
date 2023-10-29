c5840b592cb59cb025bcf83609a239904dc3d2c3
n33lp <canada0706@gmail.com>
2023-10-29 16:25:30 -0600

    adding date posted.

--- SocialDistribution/BackEnd/social_distribution_v2/posts/models.py
+++ SocialDistribution/BackEnd/social_distribution_v2/posts/models.py
@@ -1,12 +1,14 @@
+from datetime import datetime, date
 from django.db import models
-
+from django.utils import timezone 
 # Create your models here.
 class Post(models.Model):
     id = models.AutoField(primary_key=True)
     owner = models.ForeignKey('auth.User', related_name='posts', on_delete=models.CASCADE)
     content=models.CharField(max_length=4000)
     post_image=models.ImageField(upload_to="post_image",null=True,blank=True)
-    post_date=models.DateField(auto_now_add=True)
+    post_date_time = models.DateTimeField(default=timezone.now)
     category=models.CharField(max_length=3000,default=None,blank=True,null=True)
     def __str__(self):
         return str(self.id)
+
