3e03ff441330d037e8b696ff3305ba6e9a48be84
unknown <ziang6@ualberta.ca>
2023-10-30 00:59:10 -0600

    add front and backend test cases

--- SocialDistribution/BackEnd/social_distribution_v2/user_profile/tests.py
+++ SocialDistribution/BackEnd/social_distribution_v2/user_profile/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
-
+from .models import UserProfile, User
 # Create your tests here.
+class UserTestCases(TestCase):
+    def setUp(self):
+        User.objects.create(username='test', email='test@test.com', password='password')
+    def test_User_name(self):
+        user = User.objects.get(username='test')
+        self.assertEqual(user.username,'test')
