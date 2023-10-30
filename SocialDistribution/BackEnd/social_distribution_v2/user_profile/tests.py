34f0bb52746d496b7f62bbb39ed27491bfb53e98
unknown <ziang6@ualberta.ca>
2023-10-30 01:32:34 -0600

    add more cases

--- SocialDistribution/BackEnd/social_distribution_v2/user_profile/tests.py
+++ SocialDistribution/BackEnd/social_distribution_v2/user_profile/tests.py
@@ -7,3 +7,11 @@ class UserTestCases(TestCase):
     def test_User_name(self):
         user = User.objects.get(username='test')
         self.assertEqual(user.username,'test')
+
+class ProfileTestCases(TestCase):
+    def setUp(self):
+        user = User.objects.create(username='test', email='test@test.com', password='password')
+        user.save()
+        UserProfile.objects.create(owner=user)
+        profile = UserProfile.objects.get(owner=user)
+        self.assertAlmostEqual(profile.owner.username, 'test')
