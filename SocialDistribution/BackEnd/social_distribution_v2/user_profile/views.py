a55e87494e7a6e2943b5ed2b7fbfd7864207efc0
unknown <ziang6@ualberta.ca>
2023-11-14 16:46:06 -0700

    complete search backend point

--- SocialDistribution/BackEnd/social_distribution_v2/user_profile/views.py
+++ SocialDistribution/BackEnd/social_distribution_v2/user_profile/views.py
@@ -1,8 +1,8 @@
 from user_profile.models import UserProfile
-from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import viewsets, permissions, filters
 from . permissions import IsOwnerOrReadOnly
 from . serializers import ProfileSerializer
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -11,6 +11,11 @@ class ProfileViewSet(viewsets.ModelViewSet):
     serializer_class = ProfileSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly]
+    def get_queryset(self):
+        query_params = self.request.query_params
+        
+        return UserProfile.objects.filter(owner__username__contains=query_params['owner'])
+    
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
