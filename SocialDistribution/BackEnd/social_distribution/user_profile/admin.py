e6a03884355c49273ef5cfd904d117fa9af13750
Will-Boytinck <wboytinck@gmail.com>
2023-11-21 21:55:10 -0700

    deleted shit

--- SocialDistribution/BackEnd/social_distribution/user_profile/admin.py
+++
@@ -1,5 +0,0 @@
-from .models import UserProfile
-from django.contrib import admin
-
-# Register your models here.
-admin.site.register(UserProfile)
--- SocialDistribution/BackEnd/social_distribution/votes/admin.py
+++
@@ -1,5 +0,0 @@
-from .models import Vote
-from django.contrib import admin
-
-# Register your models here.
-admin.site.register(Vote)
