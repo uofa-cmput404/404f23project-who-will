0d380988ea39494d5962198bf0316da1c1b9f283
unknown <ziang6@ualberta.ca>
2023-11-22 16:53:59 -0700

    doing notifications

--- SocialDistribution/FrontEnd/social-distribution/src/pages/Notifications.jsx
+++ SocialDistribution/FrontEnd/social-distribution/src/pages/Notifications.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import Button from "../Components/Button";
-import { FaPen } from "react-icons/fa";
+import { FaPen, FaRegEnvelope, FaUserFriends } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
+import { BsSend } from "react-icons/bs";
+import { useState } from "react";
 
 const Container = styled.div`
   width: 100%;
@@ -10,7 +13,7 @@ const Container = styled.div`
 `;
 
 const SideBar = styled.div`
-  width: 20%;
+  width: 15%;
   height: 100vh;
   display: inline-block;
   text-align: center;
@@ -18,10 +21,14 @@ const SideBar = styled.div`
   justify-content: center;
   background-color: #3b3939;
   flex-direction: row;
+  /* select all children to give margin */
+  & > * {
+    margin-top: 20px;
+  }
 `;
 
-const Inbox = styled.div`
-  width: 25%;
+const MessageList= styled.div`
+  width: 30%;
   height: 100vh;
   display: flex;
 `;
@@ -33,17 +40,28 @@ const Message = styled.div`
 `;
 
 const Tile = styled.div`
-    margin: 10px;
-    height: 5%;
-    position: relative;
-    cursor: pointer;
-    align-items: center;
-    justify-content:  center;
-    display: flex;
-    border: 1px solid transparent;
+  height: 5%;
+  position: relative;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  border: 1px solid transparent;
+  background-color: transparent;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #ffffff85;
+  }
 `;
 
 const Notifications = () => {
+  const [activeSection, setActiveSection] = useState("inbox");
+
+  const handleSectionClick = (section) => {
+    setActiveSection(section);
+  };
+
   return (
     <Container>
       <SideBar>
@@ -51,11 +69,27 @@ const Notifications = () => {
           <FaPen />
           <p style={{ margin: "5px" }}>Compose</p>
         </Button>
-      <Tile>Inbox</Tile>
-      <Tile>Sent</Tile>
+        <Tile onClick={() => handleSectionClick('inbox')} active={activeSection === 'inbox'}>
+          <IconContext.Provider value={{ color: "white" }}>
+            <FaRegEnvelope />
+          </IconContext.Provider>
+          <p style={{ color: "white", margin: "5px" }}>Inbox</p>
+        </Tile>
+        <Tile onClick={() => handleSectionClick('sent')} active={activeSection === 'sent'}>
+          <IconContext.Provider value={{ color: "white" }}>
+            <BsSend />
+          </IconContext.Provider>
+          <p style={{ color: "white", margin: "5px" }}>Sent</p>
+        </Tile>
+        <Tile onClick={() => handleSectionClick('request')} active={activeSection === 'request'}>
+          <IconContext.Provider value={{ color: "white" }}>
+            <FaUserFriends />
+          </IconContext.Provider>
+          <p style={{ color: "white", margin: "5px" }}>Friend Request</p>
+        </Tile>
       </SideBar>
-      <Inbox>test</Inbox>
-      <Message>test</Message>
+      {(activeSection === 'inbox' || activeSection === 'sent') && <MessageList>test</MessageList>}
+      {(activeSection === 'inbox' || activeSection === 'sent') && <Message>test</Message>}
     </Container>
   );
 };
