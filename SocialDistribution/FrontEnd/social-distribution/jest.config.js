0021dae7bb7630619065f48ea60558fcf2301913
unknown <ziang6@ualberta.ca>
2023-11-21 21:03:00 -0700

    fix import css and axios test error.

--- SocialDistribution/FrontEnd/social-distribution/jest.config.js
+++ SocialDistribution/FrontEnd/social-distribution/jest.config.js
@@ -1,8 +1,14 @@
 module.exports = {
   transform: {
-    '^.+\.jsx?$': 'babel-jest'
+    "^.+.jsx?$": "babel-jest",
   },
-  testRegex: '(/tests/.*|(\.|/)(test|spec))\.jsx?$',
-  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
-  "testEnvironment": "jsdom"
+  testRegex: "(/tests/.*|(.|/)(test|spec)).jsx?$",
+  moduleFileExtensions: ["js", "jsx", "json", "node"],
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.js",
+    "\\.(scss|sass|css)$": "identity-obj-proxy",
+    axios: "axios/dist/node/axios.cjs",
+  },
 };
