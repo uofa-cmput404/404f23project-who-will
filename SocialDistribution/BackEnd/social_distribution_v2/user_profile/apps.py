4a7f139a16510ca5e2042e10447b460b23090456
unknown <ziang6@ualberta.ca>
2023-11-25 14:03:45 -0700

    link userprofile creation and user creation

--- SocialDistribution/BackEnd/social_distribution_v2/user_profile/apps.py
+++ SocialDistribution/BackEnd/social_distribution_v2/user_profile/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class UserProfileConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user_profile'
+    def ready(self) -> None:
+        import user_profile.signals
