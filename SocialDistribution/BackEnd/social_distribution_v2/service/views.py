f1380c5924f0b7cd1c9ce6515d10edd25f209ca6
n33lp <canada0706@gmail.com>
2023-11-25 21:14:52 -0700

    /service/authors/admin/posts/ works

--- SocialDistribution/BackEnd/social_distribution_v2/service/views.py
+++ SocialDistribution/BackEnd/social_distribution_v2/service/views.py
@@ -67,96 +67,97 @@ def all_followers(requested_author):
     return response
 
 def check_follower(author, follower):
-    print(author)
-    print(follower)
     x= all_followers(author)['items']
-    print(x)
     for i in x:
         if i['id'].split('/')[-1] == follower:
             return {'following': 'True'}
     return {'following': 'False'}
 
 def comment_to_json(comment):
-    return {1}
+    response = {
+        "type":"comment",
+        "author":author_to_json(comment.owner, UserProfile.objects.get(owner=comment.owner)),
+        "comment":comment.comment,
+        "contentType":"text/markdown",
+        "published":comment.post_date_time,
+        "id": "not done yet" #TODO: fix this
+    }
+    return response
 
 def get_comments(post):
-    # "commentsSrc": {
-    #     "type":"comments",
-    #     "page":1,
-    #     "size":5,
-    #     "post":"http://127.0.0.1:5454/authors/9de17f29c12e8f97bcbbd34cc908f1baba40658e/posts/764efa883dda1e11db47671c4a3bbd9e",
-    #     "id":"http://127.0.0.1:5454/authors/9de17f29c12e8f97bcbbd34cc908f1baba40658e/posts/de305d54-75b4-431b-adb2-eb6b9e546013/comments",
-    #     "comments":[
-    #         {
-    #             "type":"comment",
-    #             "author":{
-    #                 "type":"author",
-    #                 "id":"http://127.0.0.1:5454/authors/1d698d25ff008f7538453c120f581471",
-    #                 "url":"http://127.0.0.1:5454/authors/1d698d25ff008f7538453c120f581471",
-    #                 "host":"http://127.0.0.1:5454/",
-    #                 "displayName":"Greg Johnson",
-    #                 "github": "http://github.com/gjohnson",
-    #                 "profileImage": "https://i.imgur.com/k7XVwpB.jpeg"
-    #             },
-    #             "comment":"Sick Olde English",
-    #             "contentType":"text/markdown",
-    #             "published":"2015-03-09T13:07:04+00:00",
-    #             "id":"http://127.0.0.1:5454/authors/9de17f29c12e8f97bcbbd34cc908f1baba40658e/posts/de305d54-75b4-431b-adb2-eb6b9e546013/comments/f6255bb01c648fe967714d52a89e8e9c",
-    #         }
-    #     ]
-    # },
-    return {1}
+    id_string="http://127.0.0.1:8000/service/author/"+str(post.owner)+"/posts/"+str(post.id)
+    response = { 
+        "type":"comments",
+        "page":1,
+        "size":5, #TODO: fix this
+        "post":id_string,
+        "id":id_string,
+        "comments":[],
+    }
+    for comment in Comment.objects.all():
+        if str(comment.post) == str(post.id):
+            response['comments'].append(comment_to_json(comment))
+    return response
 
 def post_to_json(post): 
-    post = Post.objects.get(id=post.id)
+    print("here")
+    id_string="http://127.0.0.1:8000/service/author/"+str(post.owner)+"/posts/"+str(post.id)
+    comment_string ="http://127.0.0.1:8000/service/author/"+str(post.owner)+"/posts/"+str(post.id)+"/comments"
     response = {
         "type":"post",
         "title":post.title,
-        "id":f"http://127.0.0.1:8000/service/author/{post.owner}/posts/{post.id}",
+        "id": id_string,
         "source":post.source,
         "origin":post.origin,
         "description":post.description,
         "contentType":"text/plain",
         "content":post.content,
         "author":author_to_json(post.owner, UserProfile.objects.get(owner=post.owner)),
-        "categories":[post.categories],
-        "count": 1023, #TODO: fix this
-        "comments":"http://127.0.0.1:8000/service/author/{post.owner}/posts/{post.id}/comments",
-        "commentsSrc": get_comments(post),
+        # "categories": [category.category for category in post.category_part.all()]
+        "count": 0, #TODO: fix this
+        "comments":comment_string,
+        "commentsSrc": get_comments(post), #TODO: fix this
         "published":post.post_date_time,
         "visibility":post.visibility,
         "unlisted": post.post_image
-# {
-#         "unlisted":false
-#     }
     }
+    response['count'] = len(response['commentsSrc']['comments'])
     return response
 
 
 
 def all_posts(requested_author):
     all_posts = Post.objects.all()
+
     response = {'type': 'all posts', 'items': []}
     for post in all_posts:
         if str(post.owner) == str(requested_author):
             response['items'].append(post_to_json(post))
+    
     return response
     
 
 
 def GET_request(request):
-    print(request.path)
+    # print(request.path)
     print("split: ", request.path.split('/'))
-    if request.path == '/service/authors/': #done
+    response = {'status': '1'}  # Set a default value for response
+    path = request.path.split('/')
+    for i in path:
+        if i == '':
+            path.remove(i)
+    print(path)
+    if path[-1] == 'authors': #done
         response = all_authors()
-    elif request.path.split('/')[-2] == 'authors': #done
-        response = specific_author((request.path.split('/'))[-1])
-    elif request.path.split('/')[-1] == 'followers': #done
-        response = all_followers(request.path.split('/')[-2])
-    elif request.path.split('/')[-2] == 'followers': #done
-        response = check_follower(request.path.split('/')[-3],request.path.split('/')[-1])
-    elif request.path.split('/')[-1] == 'posts':
-        response = all_posts(request.path.split('/')[-2])
+    elif path[-2] == 'authors': #done
+        response = specific_author(path[-1])
+    elif path[-1] == 'followers': #done
+        response = all_followers(path[-2])
+    elif path[-2] == 'followers': #done
+        response = check_follower(path[-3],path[-1])
+    elif path[-1] == 'posts':
+        response = all_posts(path[-2])
+
     return JsonResponse(response)
 
 def POST_request(request):
