47c80f17705ecdc4e022a82368cac74aafc63fe4
xuemeng <xuemeng2@ualberta.ca>
2023-11-26 19:22:50 -0700

    delete files 2

--- SocialDistribution/FrontEnd/social-distribution/src/pages/github.jsx
+++ SocialDistribution/FrontEnd/social-distribution/src/pages/github.jsx
@@ -1,11 +1,5 @@
 import React from "react";
 import "./homepage.css"; // use css style
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogActions from '@mui/material/DialogActions';
 import { useState, useEffect } from 'react';
 
 
