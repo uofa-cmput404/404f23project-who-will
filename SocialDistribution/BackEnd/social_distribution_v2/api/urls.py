5ee12f4f0ee94c6396932bc90e447900d5b0c430
unknown <ziang6@ualberta.ca>
2023-12-01 16:25:06 -0700

    added foreign identifier

--- SocialDistribution/BackEnd/social_distribution_v2/api/urls.py
+++ SocialDistribution/BackEnd/social_distribution_v2/api/urls.py
@@ -1,15 +1,15 @@
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.routers import DefaultRouter
-from social_networking.views import UserViewSet
-from user_profile.views import ProfileViewSet
+# from social_networking.views import UserViewSet
+from user_profile.views import ProfileViewSet, CustomUserViewSet
 from posts.views import PostViewSet, CategoryViewSet
 from comments.views import CommentViewSet
 from votes.views import VoteViewSet
 from django.urls import re_path
 
 router=DefaultRouter()
-router.register(r'users',UserViewSet,basename='users')
-re_path(r'^users/.*$', UserViewSet)
+router.register(r'users',CustomUserViewSet,basename='users')
+re_path(r'^users/.*$', CustomUserViewSet)
 router.register(r'profiles',ProfileViewSet,basename='profiles')
 router.register(r'posts',PostViewSet)
 re_path(r'^posts/.*$', PostViewSet.as_view({'get': 'list', 'post': 'create'}), name='post-list'),
--- SocialDistribution/BackEnd/social_distribution_v2/user_profile/serializers.py
+++ SocialDistribution/BackEnd/social_distribution_v2/user_profile/serializers.py
@@ -8,17 +8,27 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields='__all__'
 
 class CustomUserSerializer(serializers.ModelSerializer):
-    profile = ProfileSerializer()
+    # profile = ProfileSerializer()
     class Meta:
         model = CustomUser
-        fields=('id','user_id', 'username','email','password', 'profile')
+        fields=('id','user_id', 'username','email','password',  'foreign')
         # extra_kwargs = {'password': {'write_only': True}}  # Hide password field in GET requests
 
     def create(self, validated_data):
-        user = CustomUser(
-            email = validated_data['email'],
-            username = validated_data['username']
-        )
+        if validated_data.get('foreign'):
+            user = CustomUser(
+                is_foreign = True,
+                foreign = validated_data['foreign'],
+                email = validated_data['email'],
+                username = validated_data['username']
+            )
+        else:
+            user = CustomUser(
+                is_foreign = False,
+                email = validated_data['email'],
+                username = validated_data['username']
+            )
+
         user.set_password(validated_data['password'])
         user.save()
 
