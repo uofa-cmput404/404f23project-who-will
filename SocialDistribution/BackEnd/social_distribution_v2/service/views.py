f052e3c5a4c32e52509a179f7386501496ba0719
Yanmachine <hardingiant@gmail.com>
2023-11-30 20:09:30 -0700

    Cascade comments
    Delete comments when their associated post is deleted

--- SocialDistribution/BackEnd/social_distribution_v2/service/views.py
+++ SocialDistribution/BackEnd/social_distribution_v2/service/views.py
@@ -446,7 +446,11 @@ def delete_post(path):
         post = Post.objects.get(id=post_id)
         print("Successfully retrieved post")
         try:
-            post.delete()
+            comment_ids = [comment.id for comment in post.comments.all()]
+        
+            print("Comment IDs:", comment_ids)
+            #post.delete()
+            cascade_comments(comment_ids)
             print("Successfully deleted post")
             return {'status': 'Post deleted successfully'}
         except:
@@ -455,6 +459,15 @@ def delete_post(path):
     except:
         print("Failed to retrieve database")
         return {'status': 'Could not find post in database'}
+    
+def cascade_comments(comment_ids):
+    try:
+        for c in comment_ids:
+            comments = Comment.objects.get(id=c)
+            comments.delete()
+    except:
+        print("could not delete comments")
+
 
 
 def PATCH_request(request):
