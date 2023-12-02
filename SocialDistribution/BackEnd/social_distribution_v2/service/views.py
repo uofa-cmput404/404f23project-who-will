bda79288106f34dfbaf54e78dd49d35444db922e
n33lp <canada0706@gmail.com>
2023-12-02 15:28:43 -0700

    Update URLs in views.py

--- SocialDistribution/BackEnd/social_distribution_v2/service/views.py
+++ SocialDistribution/BackEnd/social_distribution_v2/service/views.py
@@ -17,6 +17,9 @@ import json
 # for the token
 from django.middleware.csrf import get_token
 
+
+OUR_URL="https://whowill-22f35606f344.herokuapp.com"
+
 def get_csrf_token(request):
     csrf_token = get_token(request)
     return JsonResponse({"csrf_token": csrf_token})
@@ -43,9 +46,9 @@ def works(request):
 def author_to_json(user, user_profile):
     return {
         "type": "author",
-        "id": f"http://127.0.0.1:/service/author/{user.id}",
-        "url": f"http://127.0.0.1:/service/author/{user.id}",
-        "host": "http://127.0.0.1:8000/",
+        "id": f"{OUR_URL}/service/author/{user.id}",
+        "url": f"{OUR_URL}/service/author/{user.id}",
+        "host": OUR_URL,
         "displayName": f"{user.username}",
         "github": user_profile.github,
         "profileImage": user_profile.profile_image
@@ -93,7 +96,7 @@ def check_follower(author, follower):
     return {'following': 'False'}
 
 def comment_to_json(comment):
-    id_string="http://127.0.0.1:8000/service/author/"+str(comment.owner)+"/posts/"+str(comment.post.id)+"/comments/"+str(comment.id)
+    id_string=OUR_URL+"/service/author/"+str(comment.owner)+"/posts/"+str(comment.post.id)+"/comments/"+str(comment.id)
     response = {
         "type":"comment",
         "author":author_to_json(comment.owner, UserProfile.objects.get(owner=comment.owner)),
@@ -105,7 +108,7 @@ def comment_to_json(comment):
     return response
 
 def get_comments(post):
-    id_string="http://127.0.0.1:8000/service/author/"+str(post.owner)+"/posts/"+str(post.id)
+    id_string=OUR_URL+"/service/author/"+str(post.owner)+"/posts/"+str(post.id)
     response = { 
         "type":"comments",
         "page":1,
@@ -122,8 +125,8 @@ def get_comments(post):
 
 def post_to_json(post): 
     print("here")
-    id_string="http://127.0.0.1:8000/service/author/"+str(post.owner)+"/posts/"+str(post.id)
-    comment_string ="http://127.0.0.1:8000/service/author/"+str(post.owner)+"/posts/"+str(post.id)+"/comments"
+    id_string=OUR_URL+"/service/author/"+str(post.owner)+"/posts/"+str(post.id)
+    comment_string =OUR_URL+"/service/author/"+str(post.owner)+"/posts/"+str(post.id)+"/comments"
     response = {
         "type":"post",
         "title":post.title,
@@ -173,7 +176,7 @@ def get_likes_post(post):
     post_name = post.title
     post_owner = post.owner
     summary = "Likes for " + str(post_name) + " by " + str(post_owner)
-    object_string= "http://127.0.0.1:8000/service/author/"+str(post.owner)+"/posts/"+str(post.id)
+    object_string= OUR_URL+"/service/author/"+str(post.owner)+"/posts/"+str(post.id)
     response = {
         'type': 'likes',
         'summary': summary,
@@ -192,7 +195,7 @@ def get_likes_comments(comment):
     comment_name = comment.comment
     comment_owner = comment.owner
     summary = "Likes for " + str(comment_name) + " by " + str(comment_owner)
-    object_string= "http://127.0.0.1:8000/service/author/"+str(comment.owner)+"/posts/"+str(comment.id)
+    object_string= OUR_URL+"/service/author/"+str(comment.owner)+"/posts/"+str(comment.id)
     response = {
         'type': 'likes',
         'summary': summary,
@@ -219,7 +222,7 @@ def get_inbox(requested_author):
     author_profile = UserProfile.objects.get(owner=user_with_username)
     response = {
         "type": "inbox",
-        "author": f"http://127.0.0.1:8000/service/author/{requested_author}",
+        "author": f"{OUR_URL}/service/author/{requested_author}",
         "items": []
     }
     posts = Post.objects.filter(message_to=author_profile)
