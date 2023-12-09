ad59a31d25967555385b88b879bb0d0d92879e8d
unknown <ziang6@ualberta.ca>
2023-12-08 18:03:26 -0700

    modify test cases after changing backend

--- SocialDistribution/BackEnd/social_distribution_v2/comments/tests.py
+++ SocialDistribution/BackEnd/social_distribution_v2/comments/tests.py
@@ -1,15 +1,15 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
+from user_profile.models import *
 from .models import Comment
 from posts.models import Post
 
 class CommentTestCases(TestCase):
     def setUp(self):
-        user=User.objects.create(username='test', email='test@test.com', password='password')
+        user=CustomUser.objects.create(username='test', email='test@test.com', password='password')
         user.save()
         post=Post.objects.create(owner=user, content='test case', visibility='public')
         post.save()
-        user1=User.objects.create(username='test1', email='test@test.com', password='password')
+        user1=CustomUser.objects.create(username='test1', email='test@test.com', password='password')
         user1.save()
         Comment.objects.create(owner=user1, post=post, comment='nice')
     def test_Comment_fields(self):
